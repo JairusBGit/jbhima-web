84d4dbe97d6e9c00543a5e0b53bd690ac571e715
Jairus Bona <nfactordesign@gmail.com>
2022-07-05 23:52:18 +0800

    recovery?

---
+++ public/ramblings.js
@@ -0,0 +1,35 @@
+var r_text = new Array ();
+r_text[0] = "worst webdev ever";
+r_text[1] = "porter robinson enjoyer";
+r_text[2] = "hyperpop afficionado";
+r_text[3] = "i'm not giving you my sample packs";
+r_text[4] = "also check out astaelise.carrd.co";
+r_text[5] = "himawari is sunflower in japanese";
+r_text[6] = "i like smol cute things";
+r_text[7] = "my favorite color is rainbow";
+r_text[8] = "synthesizers?";
+r_text[9] = "who's joe";
+r_text[10] = "don't be racist";
+r_text[11] = "play free draw 2";
+r_text[12] = "hey all scott here";
+r_text[13] = "yes, we got gex";
+r_text[14] = 'AAAAAAAAAAAAAAAAAAAAAA-';
+r_text[15] = "mom its pronounced pomegranate";
+r_text[16] = "gif is pronounced yiff";
+r_text[17] = "you're either a gender or agender";
+r_text[18] = "unity can kiss my ass";
+r_text[19] = "fl studio > every other DAW";
+r_text[20] = "i'm thinking miku miku oo ee oo";
+r_text[21] = "*punts fidgy*";
+r_text[22] = "i have always been number one er-, two er-, three er-, one er-, wii u-";
+r_text[23] = '"womp womp" - shylily';
+r_text[24] = "you can put doom on anything!";
+r_text[25] = "lain =/= lean";
+r_text[26] = "crisp rat";
+r_text[27] = "i love sleep uwu";
+r_text[28] = "at least i did a bit better than the Sega Saturn";
+r_text[29] = "THIS ISN'T A ROBLOX GAME, THIS IS MARIO PARTY-";
+
+var i = Math.floor(30*Math.random())
+
+document.write('<span>' + r_text[i] + '</span>');
